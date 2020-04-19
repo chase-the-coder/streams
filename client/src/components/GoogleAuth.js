c2c63632b996b328cfa17753896a5aa877ee9bbd
chase lindsay <chase.lindsay1@gmail.com>
2020-04-19 01:06:45 -0400

    configuring google o auth

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export class GoogleAuth extends Component {
 	componentDidMount() {
 		window.gapi.load('client:auth2', () => {
-			window.gapi.clinet.init({
+			window.gapi.client.init({
 				clientId: '741188610952-vc77vuieth7dene72je3pubsusdvi2pq.apps.googleusercontent.com',
 				scope: 'email'
 			});
